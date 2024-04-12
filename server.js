8c050380287ec3a3a14175a806be18dc97f54e7b
sayanpal469 <sayanpal469@gmail.com>
2024-04-13 00:18:10 +0530

    something changed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const connectDb = () => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
     })
     .catch((error) => {
-      console.log(error);
+      console.log(error.message);
     });
 };
 
